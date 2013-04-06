a97cdbdb9657d7538dc213e1e2585728570963bc
Patrick Morgan <patrick@patrick-morgan.net>
2013-04-05 23:52:30 -0500

    Updated to include logger to STDOUT

--- harness.rb
+++ harness.rb
@@ -5,9 +5,11 @@
 ########################
 
 require 'rubygems'
+require 'logger'
 require 'active_record'
 
 DB_NAME="foo"
+ActiveRecord::Base.logger = Logger.new STDOUT
 ActiveRecord::Base.establish_connection(:adapter => "postgresql", :database => DB_NAME)
 
 ########################
@@ -17,11 +19,11 @@ ActiveRecord::Base.establish_connection(:adapter => "postgresql", :database => D
 
 ActiveRecord::Migration.create_table :papers do |t|
   t.string :name
-end
+end rescue puts "* Table 'papers' already exists."
 
 ActiveRecord::Migration.create_table :votes do |t|
   t.integer :paper_id
-end
+end rescue puts "* Table 'votes' already exists."
 
 class Paper < ActiveRecord::Base
   has_many :votes
@@ -31,7 +33,7 @@ class Vote < ActiveRecord::Base
   belongs_to :paper
 end
 
-10.times { Paper.create(name: "Paper #{rand(1000)}")}
-100.times { Vote.create(paper_id: rand(10) )}
+#10.times { Paper.create(name: "Paper #{rand(1000)}")}
+#100.times { Vote.create(paper_id: rand(10) )}
 
-Paper.joins(:votes).select("papers.name, COUNT(votes.id) as vote_count").group("papers.name").order("vote_count DESC")
+#Paper.joins(:votes).select("papers.name, COUNT(votes.id) as vote_count").group("papers.name").order("vote_count DESC")
